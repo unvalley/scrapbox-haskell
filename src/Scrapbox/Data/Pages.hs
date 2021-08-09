2098fe479d143820d8b560e5d08213efacd4f9cb
unvalley <kirohi.code@gmail.com>
2021-08-09 13:49:05 +0900

    initial commit

---
+++ src/Scrapbox/Data/Pages.hs
@@ -0,0 +1,34 @@
+module Scrapbox.Data.Pages where
+
+data User = User {
+  id :: String,
+  name :: String,
+  displayName :: String,
+  photo :: String
+} deriving (Show, Eq)
+
+data Line = Line {
+  id :: String,
+  text :: String,
+  userId :: String
+  created :: Int
+  updated :: Int
+} deriving (Show, Eq)
+
+data Page = Page {
+  id :: String, 
+  title :: String,
+  image :: String,
+  descriptions :: [String],
+  user :: User,
+  pin :: Int,
+  views :: Int,
+  linked :: Int,
+  commitId :: String,
+  created :: Int,
+  updated :: Int,
+  accessed :: Int,
+  snapshotCreated :: Int,
+  persistent :: Bool,
+  lines :: [Line]
+} deriving (Show, Eq)
---
+++ src/Scrapbox/Request.hs
@@ -0,0 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Scrapbox.Request where
+
+
